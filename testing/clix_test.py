1e187e55ba59f4fb7406a8a755dde099a76d88ca
Igor Detring <110975004+idetring@users.noreply.github.com>
2026-05-11 15:16:59 +0200

    Add Climatic Waterbalance (Klimatische Wasserbilanz) as indicator to CLIX (#16)
    adding the climatic waterbalance, is the difference between
    precipitation and potential evapotranspiration, to CLIX. It currently
    only uses the Hargreaves-Samani method implemented in XCLIM. This MR
    also includes minor fixes to the description of the potential
    evapotranspiration indicator.

--- testing/clix_test.py
+++ testing/clix_test.py
@@ -1,64 +1,75 @@
-import importlib.resources
-import yaml
+import unittest
 
-from pyku.resources import generate_fake_cmip6_data
 
-import xarray as xr
+class TestClixMethods(unittest.TestCase):
 
-from pyku.clix.manager import ClimateIndicator
-from pyku.clix.custom_indicators import (
-    expand_percentiles_to_daily,
-    percentile_grouped
-)
+    import importlib.resources
 
-from xclim.core.calendar import percentile_doy
+    from pyku.clix.manager import ClimateIndicator
 
-def calc_indicator(indicator_name, frequency, data):
+    def calc_indicator(self, indicator_name, frequency, data):
+        from pyku.clix.custom_indicators import (
+            expand_percentiles_to_daily,
+            percentile_grouped
+        )
+        # Define parameters
+        # (use an empty dictionary for default indicator setup)
+        clind = self.ClimateIndicator(indicator_name, frequency)
 
-    # Define parameters (use an empty dictionary for default indicator setup)
-    params = {}   
-    clind = ClimateIndicator(indicator_name, frequency)
-    
-    if clind.is_perc_indicator:
-        req_args = data.copy()
-        for varname, da in data.items():
-            arr_perc = percentile_grouped(
+        if clind.is_perc_indicator:
+            req_args = data.copy()
+            for varname, da in data.items():
+                arr_perc = percentile_grouped(
                     da,
                     group_freq=frequency,
                     per=95.,
                     climatology=True
                 )
-            arr_perc = expand_percentiles_to_daily(da, arr_perc, frequency)
-        req_args[f'{varname}_per'] = arr_perc
-    else:
-        req_args = data
+                arr_perc = expand_percentiles_to_daily(
+                        da,
+                        arr_perc,
+                        frequency
+                )
+            req_args[f'{varname}_per'] = arr_perc
+        else:
+            req_args = data
 
-    clind.required_args = req_args
-    result = clind()
-        
-    return result
+        clind.required_args = req_args
+        result = clind()
 
-# Load indicator yaml data
-# ------------------------
+        return result
 
-indicator_file = importlib.resources.files(
-    'pyku.etc') / 'climate_indicators.yaml'
+    def test_all_indicators(self):
+        import pyku
+        from pyku.resources import generate_fake_cmip6_data
+        # Load indicator data
+        # ------------------------
 
-with open(indicator_file) as f:
-    grouped_indicator_data = yaml.safe_load(f)
+        grouped_indicator_data = pyku.PYKU_RESOURCES.load_resource(
+                    'climate_indicators'
+                )
 
-# Derive dictionary with variables as keys and list of indicator names
-indicator_data = {
-    group: list(indicators.keys())
-    for group, indicators in grouped_indicator_data.items()
-}
+        # Derive dictionary with variables as keys and list of indicator names
+        indicator_data = {
+            group: list(indicators.keys())
+            for group, indicators in grouped_indicator_data.items()
+        }
 
-for varnames, indicators in indicator_data.items():
-    data = {}
-    #print(f'Variable(s) to use for caculation: {', '.join(varnames.split('+'))}')
-    for varname in varnames.split('+'):
-        data[varname] = generate_fake_cmip6_data(
-            variable=varname, ntime=2, nlat=1, nlon=1, freq='D')[varname]
-    for indicator_name in indicators:
-        #print(indicator_name)
-        calc_indicator(indicator_name, 'YS', data)
+        for varnames, indicators in indicator_data.items():
+            data = {}
+            for varname in varnames.split('+'):
+                data[varname] = generate_fake_cmip6_data(
+                    variable=varname,
+                    ntime=2,
+                    nlat=1,
+                    nlon=1,
+                    freq='D'
+                )[varname]
+
+            for indicator_name in indicators:
+                print(f"Testing indicator {indicator_name}")
+                self.calc_indicator(indicator_name, 'YS', data)
+
+
+if __name__ == '__main__':
+    unittest.main()
